302535874c48f0495649cb2bec6431e5e6340f19
wajari <wajari@gmail.com>
2020-03-21 20:31:20 +0100

    Meme generator with simple API from imgflip and class component

---
+++ meme/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header'
+import MemeGenerator from './MemeGenerator'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
